9caa27941d24e08ef915c4ffdd96e873024d7080
Nhi To Kieu <nhito@192.168.1.11>
2026-03-15 12:23:52 +0700

    feat(add-to-cart): Add negative API test cases

--- tests/api/add-to-cart.api.spec.ts
+++ tests/api/add-to-cart.api.spec.ts
@@ -27,7 +27,50 @@ test.describe('API - Add to Cart', () => {
     expect(viewCartResponseBody.Items[0].prod_id).toBe(product.id);
   });
 
-  test.afterEach(async ({ request }) => {
-    await resetCart(request, user.userName);
+  test('Add product to cart successfully with flag = `false`', async ({ request }) => {
+    const payload: AddToCartPayload = {
+      token: user.token,
+      productId: product.id,
+      flag: false,
+    };
+    const resp = await addProductToCart(request, payload);
+    expect(resp.status()).toBe(200);
+
+    const viewCartResponse = await viewCart(request, user.token);
+    expect(viewCartResponse.status()).toBe(200);
+    const viewCartResponseBody = await viewCartResponse.json();
+    expect(viewCartResponseBody.Items).toHaveLength(1);
+    expect(viewCartResponseBody.Items[0].prod_id).toBe(product.id);
+  });
+
+  test('Add product to cart unsuccessfully with invalid cookie', async ({ request }) => {
+    const payload: AddToCartPayload = {
+      token: 'invalid-token',
+      productId: product.id,
+    };
+    const resp = await addProductToCart(request, payload);
+    expect(resp.status()).toBe(200);
+
+    const respBody = await resp.json();
+    expect(respBody['errorMessage']).toEqual('Bad parameter, token malformed.');
+  });
+
+  test('Add product to cart unsuccessfully with empty cookie', async ({ request }) => {
+    const payload: AddToCartPayload = {
+      token: '',
+      productId: product.id,
+    };
+    const resp = await addProductToCart(request, payload);
+    expect(resp.status()).toBe(500);
+  });
+
+  test('Add product to cart unsuccessfully with empty id', async ({ request }) => {
+    const payload: AddToCartPayload = {
+      id: '',
+      token: user.token,
+      productId: '',
+    };
+    const resp = await addProductToCart(request, payload);
+    expect(resp.status()).toBe(500);
   });
 });
--- tests/api/create-order.api.spec.ts
+++ tests/api/create-order.api.spec.ts
@@ -1,18 +1,13 @@
 import { test, expect } from '@playwright/test';
 import { addProductToCart, resetCart } from '../../api/cart.api';
 import { userData, productData } from '../../data';
-import { AddToCartPayload } from '../../api/types';
 
 const user = userData.validData.validAccount;
 const product = productData.products[0];
 
 test.describe('API - Create Order', () => {
   test('Create order successfully', async ({ request }) => {
-    const payload: AddToCartPayload = {
-      token: user.token,
-      productId: product.id,
-    };
-    await addProductToCart(request, payload);
+    await addProductToCart(request, {token: user.token, productId: product.id});
     const response = await resetCart(request, user.userName);
     expect(response.status()).toBe(200);
   });
